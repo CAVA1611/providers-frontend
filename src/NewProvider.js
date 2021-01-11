ef8bd24155bfc88e12aee832da2396d77cdc9561
Cesar Velasco <cesar091611pm@gmail.com>
2021-01-11 07:10:53 +0100

    Added support for NEW, DELETE & EDIT Provider

---
+++ src/NewProvider.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+class NewProvider extends React.Component {
+    constructor(props){
+    super(props);
+    this.state = {cif: '', name:'', addess: '', cp: '', phone:'', email:''};
+    this.changeProvider = this.changeProvider.bind(this);
+    this.clickAdd = this.clickAdd.bind(this);
+    }   
+
+    changeProvider(event){
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        });
+
+    }
+
+
+    clickAdd() {
+        this.props.onAddProvider(this.state);
+        this.setState({
+            cif: '', name:'', addess: '', cp: '', phone:'', email:''
+        });
+
+    }
+
+
+
+    render() {
+        return(
+            <tr>
+                <td><input className="form-control" name="cif" value={this.state.cif} onChange={this.changeProvider}></input></td>
+                <td><input className="form-control" name="name" value={this.state.name} onChange={this.changeProvider}></input></td>
+                <td><input className="form-control" name="address" value={this.state.address} onChange={this.changeProvider}></input></td>
+                <td><input className="form-control" name="cp" value={this.state.cp} onChange={this.changeProvider}></input></td>
+                <td><input className="form-control" name="phone" value={this.state.phone} onChange={this.changeProvider}></input></td>
+                <td><input className="form-control" name="email" value={this.state.email} onChange={this.changeProvider}></input></td>
+                <td><button className="btn btn-primary" onClick={this.clickAdd}>Add Provider</button></td>
+            </tr>
+        );
+
+    }
+
+
+}
+
+export default NewProvider;
--- src/Providers.js
+++ src/Providers.js
@@ -1,28 +1,96 @@
 import React from   'react';
 import Provider from './Provider.js';
 import Alert from './Alert.js';
+import NewProvider from './NewProvider.js';
+import EditProvider from './EditProvider.js';
+
 
 class Providers extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            selectedProvider: null
+            errorInfo: null,
+            providers: this.props.providers,
+            isEditing: {}
         }
         this.handleEdit = this.handleEdit.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
         this.handleCloseError = this.handleCloseError.bind(this);
+        this.addProvider = this.addProvider.bind(this);
     }
 
     handleEdit(provider){
-        this.setState({
-            selectedProvider: provider
+        this.setState(prevState => ({
+            isEditing: {...prevState.isEditing, [provider.name]: provider}
+        }));
+    }
+
+    handleDelete(provider){
+        this.setState(prevState => {
+            providers: prevState.providers.filter((c) => c.name !== provider.name)
+        })
+
+    }
+
+    handleCancel(name, provider) {
+        this.setState(prevState => {
+            const isEditing = Object.assign({}, prevState.isEditing);
+            delete isEditing[name];
+            return{
+                isEditing: isEditing
+            }
+        })
+    }
+
+    handleChange(name, provider){
+        this.setState(prevState => ({
+            isEditing: {...prevState.isEditing, [name]: provider}
+        }))
+    }
+
+    handleSave(name, provider){
+        this.setState(prevState => {
+            const isEditing = Object.assign({}, prevState.isEditing);
+            delete isEditing[name];
+
+            if(name === provider.name) {
+                const providers = prevState.providers;
+                const pos = providers.findIndex(c => c.name === provider.name);
+                return{
+                    providers: [...providers.slice(0,pos), Object.assign({}, provider), ...providers.slice(pos+1)],
+                    isEditing: isEditing
+
+                }
+            }
+
+            return{
+                errorInfo: "cannot edit"
+            }
+
         });
+
     }
 
     handleCloseError(){
         this.setState({
-            selectedProvider: null
+            errorInfo: null
         });
 
+    }
+
+
+    addProvider(provider) {
+        this.setState(prevState => {
+            const providers = prevState.providers;
+            if(!providers.find(c=> c.name === provider.name)) {
+                return({
+                    providers: [...prevState.providers, provider]
+                });
+            }
+            return ({
+                errorInfo: 'Provider already exist'
+            });
+        });
     }
 
 
@@ -33,7 +101,7 @@ class Providers extends React.Component{
     
         return(
             <div>
-                <Alert message={this.state.selectedProvider} onClose={this.handleCloseError}/>
+                <Alert message={this.state.errorInfo} onClose={this.handleCloseError}/>
                 <table class="table">
                 <thead>
                     <tr>
@@ -43,11 +111,21 @@ class Providers extends React.Component{
                     <th>CODIGO POSTAL</th>
                     <th>PHONE</th>
                     <th>E-MAIL</th>
+                    <th>&nbsp;</th>
                     </tr>
                 </thead>
-                    {this.props.providers.map((provider) =>
-                    <Provider provider={provider} onEdit={this.handleEdit}/>
-                )}
+                <NewProvider onAddProvider={this.addProvider}/>
+                    {this.state.providers.map((provider) =>
+                    ! this.state.isEditing[provider.name] ?
+                    <Provider key={provider.name} provider={provider} 
+                        onEdit={this.handleEdit}
+                        onDelete={this.handleDelete}/>
+                    :
+                    <EditProvider key={provider.name} provider={this.state.isEditing[provider.name]} 
+                    onCancel={this.handleCancel.bind(this, provider.name)}
+                    onChange={this.handleChange.bind(this, provider.name)}
+                    onSave={this.handleSave.bind(this, provider.name)}/>
+                    )}
 
 
 
