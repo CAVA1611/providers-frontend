c22a73927e55c210d578064d5c759b13818b80e6
Cesar Velasco <cesar091611pm@gmail.com>
2021-01-11 08:15:42 +0100

    Added API interaction

--- src/App.js
+++ src/App.js
@@ -5,31 +5,11 @@ import Providers from './Providers.js';
 
 
 function App() {
-  const providers = [{
-    cif: "A89325672", 
-    name: "Proveedor 4", 
-    address: "Barcelona", 
-    cp:"54324", 
-    phone: "0911111324",
-    email:"provedor4@gmail.com",
-    //code: "IJK101", 
-    //stock_sale: "200"
-  },
-  {
-    cif: "A65321782", 
-    name: "Proveedor 3", 
-    address: "Malaga", 
-    cp:"18945", 
-    phone: "0987690000", 
-    email:"provedor3@gmail.com",
-    //code: "FGH789", 
-    //stock_sale": "200"}
-
-  }];
+  
   return (
     <div>
     <h1>PROVIDERS INFO</h1>
-    <Providers providers = {providers}/>
+    <Providers />
     </div>
   );
 }
